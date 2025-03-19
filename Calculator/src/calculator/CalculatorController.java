3db830ef56d2e69f8a004206b4088dd76148426f
Naol <naolmes15062015@gmail.com>
2025-03-20 02:10:02 +0300

    version1

---
+++ Calculator/src/calculator/CalculatorController.java
@@ -0,0 +1,16 @@
+
+package calculator;
+
+import javafx.event.ActionEvent;
+
+
+public class CalculatorController {
+    public void processNumbers(ActionEvent event){
+        
+    }
+    
+    public  void processOperators(ActionEvent event) {
+        
+    }
+    
+}
---
+++ Calculator/src/calculator/Modal.java
@@ -0,0 +1,12 @@
+package calculator;
+
+
+public class Modal {
+    public float  calculate(long number1, long number2, String operator){
+        
+        
+        
+        return 0;
+    }
+    
+}
