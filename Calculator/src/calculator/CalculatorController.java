9dc122c5d41a8589d5835d28cd65ff7112651886
Naol <naolmes15062015@gmail.com>
2025-03-20 11:20:35 +0300

    version3

--- Calculator/src/calculator/CalculatorController.java
+++ Calculator/src/calculator/CalculatorController.java
@@ -2,19 +2,49 @@ package calculator;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class CalculatorController {
 
     @FXML
     private Label result;
+    private long number1;
+    private String operator = "";
+    private boolean start = true;
 
+    @FXML
     public void processNumbers(ActionEvent event) {
+        if (start) {
+            result.setText("");
+            start = false;
+        }
+        String value = ((Button) event.getSource()).getText();
 
     }
 
+    @FXML
     public void processOperators(ActionEvent event) {
-
+        String value = ((Button)event.getSource()).getText();
+        if (!Boolean.valueOf(equals("="))) {
+            if (!operator.isEmpty()) {
+                return;
+            }
+            operator = value;
+            number1 = Long.parseLong(result.getText());
+            result.setText("");
+        }
+        else{
+            if(operator.isEmpty()){
+                return;
+            }
+                long number2 = Long.parseLong(result.getText());
+                float output = model.calculate(number1, number2, operator);
+                result.setText(String.valueOf(output));
+                operator = "";
+                start= true;
+            
+        }
     }
 
 }
